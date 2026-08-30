4945e43d932ed06d495af89645bc92dfd068db48
nohseongmin <sm9375693@gmail.com>
2026-08-31 01:50:06 +0900

    Docs: hello.go 실행 안내 주석의 폴더명 대소문자 오기 수정

--- 1-Hello/hello.go
+++ 1-Hello/hello.go
@@ -7,6 +7,6 @@ func main() {
 	fmt.Println("Hello, World!")
 }
 
-//terminal 에서 go run .\1-hello\hello.go 로 실행
+//terminal 에서 go run .\1-Hello\hello.go 로 실행 (폴더명 대소문자 그대로: 1-Hello)
 //launch.json이 상위 폴더에 있으므로 위치를 명확히 해줄것
 //Lets 'go' my man
